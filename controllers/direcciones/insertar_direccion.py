5048ee343afa88b42609c948d000d1bee69472a9
Salvador HM <salvadorhm@mba.local>
2026-07-13 13:22:25 -0600

    CREATED nueva direccion funcional

---
+++ controllers/direcciones/insertar_direccion.py
@@ -0,0 +1,62 @@
+import web
+import sqlite3
+
+render = web.template.render("views/direcciones", base="layout_direcciones")
+
+class InsertarDireccion:
+
+    def insertarDireccion(self, registro: dict) -> bool:
+        try:
+            conn = sqlite3.connect("sql/agenda.db")
+            cursor = conn.cursor()
+            query = """
+                INSERT INTO direcciones (
+                    id_contacto,
+                    pais,
+                    estado,
+                    ciudad,
+                    colonia,
+                    calle,
+                    numero_exterior
+                )
+                VALUES (?, ?, ?, ?, ?, ?, ?);
+            """
+            datos = [
+                registro['id_contacto'],
+                registro['pais'],
+                registro['estado'],
+                registro['ciudad'],
+                registro['colonia'],
+                registro['calle'],
+                registro['numero_exterior']
+            ]
+            cursor.execute(query, datos)
+            conn.commit()
+            return True
+        except sqlite3.Error as error:
+            print(f"ERROR ModelDirecciones insertar: {error.args}")
+            return False
+        except Exception as error:
+            print(f"ERROR ModelDirecciones insertar: {error.args}")
+            return False
+        finally:
+            if 'conn' in locals() and conn:
+                conn.close()
+
+
+    def GET(self,id_contacto:int):
+        return render.insertar_direccion(id_contacto)  # type: ignore
+
+    def POST(self,id_contacto:int):
+        formulario = web.input()
+        registro = {
+            'id_contacto': formulario['id_contacto'],
+            'pais': formulario['pais'],
+            'estado': formulario['estado'],
+            'ciudad': formulario['ciudad'],
+            'colonia': formulario['colonia'],
+            'calle': formulario['calle'],
+            'numero_exterior': formulario['numero_exterior'],
+        }
+        response = self.insertarDireccion(registro)
+        raise web.seeother(f"/ver_contacto/{formulario['id_contacto']}")
