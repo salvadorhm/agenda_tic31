c6f7ea350c5c04b612822ecfcaf18eed6ca14595
Salvador HM <salvadorhm@mba.local>
2026-07-13 10:47:38 -0600

    CREATE borrar_contacto funcional

--- controllers/contactos/borrar_contacto.py
+++ controllers/contactos/borrar_contacto.py
@@ -5,6 +5,23 @@ render = web.template.render('views/contactos', base='layout')
 
 class BorrarContacto:
 
+    def eliminarContacto(self, id_contacto:int):
+        try:
+            conexion = sqlite3.connect("sql/agenda.db")
+            conexion.row_factory = sqlite3.Row
+            cursor = conexion.cursor()
+            query = "DELETE FROM contactos WHERE id_contacto = ?"
+            cursor.execute(query,(id_contacto,))
+            conexion.commit()
+            conexion.close()
+            return True
+        except sqlite3.Error as error:
+            print(f"ERROR 102: {error.args}")
+            return False
+        except Exception as error:
+            print(f"ERROR 103: {error.args}")
+            return False
+
     def buscarContacto(self, id_contacto:int):
         try:
             conexion = sqlite3.connect("sql/agenda.db")
@@ -27,12 +44,18 @@ class BorrarContacto:
             return contacto
         except sqlite3.Error as error:
             print(f"ERROR 102: {error.args}")
-            return []
+            return {}
         except Exception as error:
             print(f"ERROR 103: {error.args}")
-            return []
+            return {}
 
     def GET(self,id_contacto:int):
         print(f"ID_CONTACTO: {id_contacto}")
         contacto = self.buscarContacto(id_contacto)
-        return render.borrar_contacto(contacto)
+        return render.borrar_contacto(contacto) # type: ignore
+
+    def POST(self,id_contacto:int):
+        resultado = self.eliminarContacto(id_contacto)
+        web.ctx.status = '303 See Other'
+        web.header('Location', '/lista_contactos')
+        return ''
