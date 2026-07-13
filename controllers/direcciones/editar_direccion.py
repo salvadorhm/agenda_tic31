74bf036bd3acafff9617f44428317ac9ff850beb
Salvador HM <salvadorhm@mba.local>
2026-07-13 13:09:45 -0600

    CREATED ver direccion funcional

---
+++ controllers/direcciones/editar_direccion.py
@@ -0,0 +1,96 @@
+import web
+import sqlite3
+
+render = web.template.render("views/direcciones", base="layout_direcciones")
+
+
+class EditarDireccion:
+
+    def buscarDireccion(self, id_direccion: int) -> dict:
+        try:
+            conn = sqlite3.connect("sql/agenda.db")
+            cursor = conn.cursor()
+            query = "SELECT * FROM direcciones WHERE id_direccion = ?"
+            cursor.execute(query, (id_direccion,))
+            row = cursor.fetchone()
+            if not row:
+                return {}
+            registro = {
+                "id_direccion": row[0],
+                "id_contacto": row[1],
+                "pais": row[2],
+                "estado": row[3],
+                "ciudad": row[4],
+                "colonia": row[5],
+                "calle": row[6],
+                "numero_exterior": row[7],
+            }
+            conn.close()
+            return registro
+        except sqlite3.Error as error:
+            print(f"ERROR EditarDireccion buscar: {error.args}")
+            return {}
+        except Exception as error:
+            print(f"ERROR EditarDireccion buscar: {error.args}")
+            return {}
+
+    def actualizarDireccion(self, registro: dict) -> bool:
+        try:
+            conn = sqlite3.connect("sql/agenda.db")
+            cursor = conn.cursor()
+            query = """
+                UPDATE direcciones
+                SET
+                    id_contacto = ?,
+                    pais = ?,
+                    estado = ?,
+                    ciudad = ?,
+                    colonia = ?,
+                    calle = ?,
+                    numero_exterior = ?
+                WHERE id_direccion = ?;
+            """
+            datos = [
+                registro["id_contacto"],
+                registro["pais"],
+                registro["estado"],
+                registro["ciudad"],
+                registro["colonia"],
+                registro["calle"],
+                registro["numero_exterior"],
+                registro["id_direccion"],
+            ]
+            cursor.execute(query, datos)
+            conn.commit()
+            return True
+        except sqlite3.Error as error:
+            print(f"ERROR ModelDirecciones actualizar: {error.args}")
+            return False
+        except Exception as error:
+            print(f"ERROR ModelDirecciones actualizar: {error.args}")
+            return False
+        finally:
+            if "conn" in locals() and conn:
+                conn.close()
+
+    def GET(self, id_direccion):
+        response = self.buscarDireccion(id_direccion)
+        return render.editar_direccion(response)  # type: ignore
+
+    def POST(self, id_direccion):
+        formulario = web.input()
+        registro = {
+            "id_direccion": id_direccion,
+            "id_contacto": formulario["id_contacto"],
+            "pais": formulario["pais"],
+            "estado": formulario["estado"],
+            "ciudad": formulario["ciudad"],
+            "colonia": formulario["colonia"],
+            "calle": formulario["calle"],
+            "numero_exterior": formulario["numero_exterior"],
+        }
+        response = self.actualizarDireccion(registro)
+        web.ctx.status = '303 See Other'
+        web.header('Location', f"/ver_contacto/{formulario['id_contacto']}")
+        return ''
+        #raise web.seeother(f"/ver_contacto/{formulario['id_contacto']}")
